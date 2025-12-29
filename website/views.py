d4900b4d6f6dacba7dabe69702543a1c863e06ea
Mav <riherr00@gmail.com>
2025-12-29 15:26:10 +0300

    Deleting tasks

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+import json
 
 from website import db
 from website.models import Note
@@ -20,3 +21,15 @@ def home():
             db.session.commit()
             flash('Note added', category='success')
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    data = json.loads(request.data)
+    note_id = data['noteId']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
