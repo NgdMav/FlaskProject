a3a913f6279e74d8db4b32ee3ff90325e309a43b
Mav <riherr00@gmail.com>
2025-12-29 14:15:47 +0300

    Log in, log out and login req with flask_login

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager, login_manager
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -22,6 +23,14 @@ def create_app():
     with app.app_context():
         create_database()
         print('Database created')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
 
 from . import db
 from .models import User
@@ -16,13 +17,18 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Login successful', 'success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
         flash('Incorrect email or password', 'error')
 
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return '<p>Logout</p>'
+    logout_user()
+    flash('You have been logged out', 'success')
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
@@ -50,6 +56,7 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', 'success')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
     return render_template("signup.html")
